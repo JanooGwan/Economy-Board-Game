eab258035e35856cbe274cb6fe62cb6d08bc4f5e
JanooGwan <janoogwan39@gmail.com>
2024-07-22 16:56:00 +0900

    Member Entity 작성

---
+++ src/main/java/com/example/EconomyBoardGame/entity/member.java
@@ -0,0 +1,36 @@
+package com.example.EconomyBoardGame.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String introduction;
+
+    @Column(nullable = false)
+    private int gold = 0;
+
+    @Column(nullable = false)
+    private int manpowerLow = 0;
+
+    @Column(nullable = false)
+    private int manpowerMedium = 0;
+
+    @Column(nullable = false)
+    private int manpowerHigh = 0;
+
+    @Column(nullable = false)
+    private int minepower = 0;
+
+    // Getters and setters
+}
