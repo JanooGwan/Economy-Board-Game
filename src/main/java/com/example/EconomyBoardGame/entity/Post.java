16b8b44143675b1a05ce05723420d7d80a8dd94a
JanooGwan <janoogwan39@gmail.com>
2024-07-25 15:58:47 +0900

    Member, Post Entity 작성

--- src/main/java/com/example/EconomyBoardGame/entity/Post.java
+++ src/main/java/com/example/EconomyBoardGame/entity/Post.java
@@ -1,4 +1,36 @@
 package com.example.EconomyBoardGame.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = true, length = 5000)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "writer_id")
+    private Member writer;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @Column(nullable = false)
+    private LocalDateTime createdDate;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedDate;
 }
